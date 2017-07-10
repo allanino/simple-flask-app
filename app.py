3f844de552f8c5f314d0c2cba762a9b5c6db1a14
Allan Costa <allaninocencio@yahoo.com.br>
2017-07-10 10:29:33 -0300

    Fix PEP8 linter warnings

--- app.py
+++ app.py
@@ -1,20 +1,17 @@
-
-from flask import Flask, request, url_for, render_template, redirect, abort, session
-import requests
+from flask import Flask
 
 app = Flask(__name__)
+
 
 @app.route('/ping')
 def ping():
     return 'OK', 200
 
+
 @app.route('/')
 def index():
     return 'Test', 200
 
-@app.route('/docs')
-def docs():
-    return 'Press alt+F4 to access the true docs.', 200
 
 if __name__ == '__main__':
     app.run('0.0.0.0', 5000, debug=True)
